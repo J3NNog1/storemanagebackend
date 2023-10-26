c965ae8273a6551785c62eaa2702121cee825782
Jenn <jenniferjuarezjj@gmail.com>
2023-10-25 23:54:48 -0400

    create store model

--- src/main/java/com/jenncodes/storemanagementspringbootbackend/model/Store.java
+++ src/main/java/com/jenncodes/storemanagementspringbootbackend/model/Store.java
@@ -1,4 +1,29 @@
 package com.jenncodes.storemanagementspringbootbackend.model;
 
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "Store")
 public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String storeName;
+    private String location;
+    private String contactInfo;
+    private String storeType;
+    private String openingDate;
+
+
 }
