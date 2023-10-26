c4b5f2d3f34c43f3501c67a9d10890c48e377083
Jenn <jenniferjuarezjj@gmail.com>
2023-10-26 00:08:17 -0400

    create item management model

--- src/main/java/com/jenncodes/storemanagementspringbootbackend/model/ItemManagement.java
+++ src/main/java/com/jenncodes/storemanagementspringbootbackend/model/ItemManagement.java
@@ -1,4 +1,36 @@
 package com.jenncodes.storemanagementspringbootbackend.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "itemmanagement")
 public class ItemManagement {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name= "item_name")
+    private String itemName;
+    @Column(name= "description")
+    private String description;
+    @Column(name= "first_name")
+    private String category;
+    @Column(name= "first_name")
+    private double price;
+    @Column(name= "first_name")
+    private double initialQuantity;
+    //maybe use float
+
+
+
 }
--- src/main/java/com/jenncodes/storemanagementspringbootbackend/model/Store.java
+++ src/main/java/com/jenncodes/storemanagementspringbootbackend/model/Store.java
@@ -19,10 +19,15 @@ public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name= "store_name")
     private String storeName;
+    @Column(name= "location")
     private String location;
+    @Column(name= "contact_info")
     private String contactInfo;
+    @Column(name= "store_type")
     private String storeType;
+    @Column(name= "opening_date")
     private String openingDate;
 
 
