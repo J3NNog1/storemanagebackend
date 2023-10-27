3d31c166e6e2318e9f9bfde3fb528b2440878410
Jenn <jenniferjuarezjj@gmail.com>
2023-10-26 22:11:11 -0400

    create CRUD method for StoreController, showing in both my postman and phpmyadmin

--- src/main/java/com/jenncodes/storemanagementspringbootbackend/controller/StoreController.java
+++ src/main/java/com/jenncodes/storemanagementspringbootbackend/controller/StoreController.java
@@ -1,11 +1,12 @@
 package com.jenncodes.storemanagementspringbootbackend.controller;
 
+import com.jenncodes.storemanagementspringbootbackend.exception.ResourceNotFoundException;
 import com.jenncodes.storemanagementspringbootbackend.model.Store;
 import com.jenncodes.storemanagementspringbootbackend.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +19,37 @@ public class StoreController {
     @GetMapping
     public List<Store> getAllStores() {
         return storeRepository.findAll();
-
     }
+    //below i created a post method for adding to my database
+    @PostMapping
+    public Store createStore(@RequestBody Store store) {
+        return storeRepository.save(store);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Store> getStoreById(@PathVariable long id){
+        Store store = storeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Store does not exist with this id:" + id));
+        return ResponseEntity.ok(store);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Store> updateStore(@PathVariable long id, @RequestBody Store storeDetails) {
+        Store updateStore = storeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Store does not exist with this id:" + id ));
+        updateStore.setStoreType(storeDetails.getStoreType());
+        updateStore.setStoreName(storeDetails.getStoreName());
+        updateStore.setLocation(storeDetails.getLocation());
+        updateStore.setContactInfo(storeDetails.getContactInfo());
+        updateStore.setOpeningDate(storeDetails.getOpeningDate());
+        storeRepository.save(updateStore);
+        return ResponseEntity.ok(updateStore);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteStore(@PathVariable long id) {
+        Store store = storeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Store does not exist with this id: " + id ));
+        storeRepository.delete(store);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
 }
